5127e280ecd6d15da63e3a663ce3b7017ec3d6d5
“msun5040 <michael_h_sun@brown.edu>
2024-05-24 12:43:40 -0400

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by Michael Sun on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
